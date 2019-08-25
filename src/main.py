2855cb11eb6999118ed3f60b7ae1aaff40a57f70
Hugo Kiiski <kiiskihugo@gmail.com>
2019-08-25 15:51:04 +0300

    Async io

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
-from .sensor import sense, cleanup
+from sensor import sense, cleanup
+import asyncio
 
-def main():
+
+async def main():
     try:
-        sense(on_sit_down, on_get_up)
+        await sense(on_sit_down, on_get_up)
     except KeyboardInterrupt:
         pass
     finally:
         cleanup()
 
+
 def on_sit_down():
+    print("Sit down")
     pass
+
 
 last_session_start_time = None
-def on_get_up():
-    pass
 
+
+def on_get_up():
+    print("Stand down")
+    pass
--- src/sensor.py
+++ src/sensor.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 import time
+import asyncio
 
 INPUT_PIN = 4
 SITTING_THRESHOLD = 100
 
-def sense(on_sit_down, on_get_up):
+
+async def sense(on_sit_down, on_get_up):
     GPIO.setmode(GPIO.BCM)
 
     sitting_down = False
     while True:
-        time = rc_time(INPUT_PIN)
+        time = await rc_time(INPUT_PIN)
         if time < SITTING_THRESHOLD:
             if sitting_down == False:
                 on_sit_down()
@@ -19,22 +21,26 @@ def sense(on_sit_down, on_get_up):
                 on_get_up()
             sitting_down = False
 
+
 def cleanup():
     GPIO.cleanup()
 
-def rc_time(pin_to_circuit):
+
+async def rc_time(pin_to_circuit):
     tick = 0
 
-    #Output on the pin for
+    # Output on the pin for
     GPIO.setup(pin_to_circuit, GPIO.OUT)
     GPIO.output(pin_to_circuit, GPIO.LOW)
-    time.sleep(0.1)
+    await asyncio.sleep(0.1)
 
-    #Change the pin back to input
+    # Change the pin back to input
     GPIO.setup(pin_to_circuit, GPIO.IN)
 
-    #Count until the pin goes high
+    # Count until the pin goes high
     while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        print("Tick")
         tick += 1
+        await asyncio.sleep(0.01)
 
     return tick
